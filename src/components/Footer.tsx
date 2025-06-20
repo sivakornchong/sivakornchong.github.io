d8b0700a1315fe29cba3021e72f55fb15535ac46
oak <sivakornchong@gmail.com>
2025-06-19 21:50:36 -0700

    update readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,6 @@ function Footer() {
         <a href="https://github.com/sivakornchong" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/sivakornchong" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>Credit: portfolio template from <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a></p>
     </footer>
   );
 }
